6d474c4cb866e8f8414ab921a47221d8c8e80b03
Bjørn Ivar Teigen <bjorn@domos.no>
2023-10-20 10:33:02 +0200

    Formatting

--- include/utils.hpp
+++ include/utils.hpp
@@ -13,7 +13,7 @@
 #include <sstream>
 #include "packets.h"
 
-#define HDR_TTL		255         /* TTL=255 in TWAMP for IP Header */
+#define HDR_TTL 255 /* TTL=255 in TWAMP for IP Header */
 #define SERVER_PORT 862
 #define CHECK_TIMES 100
 
@@ -24,14 +24,13 @@ typedef struct ip_header {
     uint8_t tos;
 } IPHeader;
 
-
-void timeval_to_timestamp(const struct timeval *tv, Timestamp * ts);
+void timeval_to_timestamp(const struct timeval *tv, Timestamp *ts);
 void timespec_to_timestamp(const struct timespec *tv, Timestamp *ts);
 
-void timestamp_to_timeval(const Timestamp * ts, struct timeval *tv);
+void timestamp_to_timeval(const Timestamp *ts, struct timeval *tv);
 
-uint64_t timestamp_to_usec(const Timestamp * ts);
-uint64_t timestamp_to_nsec(const Timestamp * ts);
+uint64_t timestamp_to_usec(const Timestamp *ts);
+uint64_t timestamp_to_nsec(const Timestamp *ts);
 struct timespec nanosecondsToTimespec(uint64_t delay_epoch_nanoseconds);
 uint64_t get_usec();
 Timestamp get_timestamp();
@@ -41,26 +40,26 @@ void set_socket_options(int socket, uint8_t ip_ttl, uint8_t timeout_secs);
 void set_socket_tos(int socket, uint8_t ip_tos);
 void get_kernel_timestamp(msghdr message, timespec *ts);
 bool isWithinEpsilon(double a, double b, double percentEpsilon);
-template <class T>
-std::string vectorToString(std::vector<T> vec, std::string sep){
+template <class T> std::string vectorToString(std::vector<T> vec, std::string sep)
+{
     std::stringstream result;
     std::copy(vec.begin(), vec.end(), std::ostream_iterator<T>(result, sep.c_str()));
-    return result.str().substr(0, result.str().size()-1);
+    return result.str().substr(0, result.str().size() - 1);
 }
-template<typename Iter, typename RandomGenerator>
-Iter select_randomly(Iter start, Iter end, RandomGenerator& g) {
+template <typename Iter, typename RandomGenerator> Iter select_randomly(Iter start, Iter end, RandomGenerator &g)
+{
     std::uniform_int_distribution<> dis(0, std::distance(start, end) - 1);
     std::advance(start, dis(g));
     return start;
 }
 
-template<typename Iter>
-Iter select_randomly(Iter start, Iter end, uint32_t seed=0) {
+template <typename Iter> Iter select_randomly(Iter start, Iter end, uint32_t seed = 0)
+{
     static std::random_device rd;
-    static std::mt19937 gen(seed==0 ? rd() : seed);
+    static std::mt19937 gen(seed == 0 ? rd() : seed);
     return select_randomly(start, end, gen);
 }
 Timestamp ntohts(Timestamp ts);
 Timestamp htonts(Timestamp ts);
-bool parseIPPort(const std::string& input, std::string& ip, uint16_t& port);
-#endif //DOMOS_TWAMP_LIGHT_TWAMP_LIGHT_HPP
+bool parseIPPort(const std::string &input, std::string &ip, uint16_t &port);
+#endif // DOMOS_TWAMP_LIGHT_TWAMP_LIGHT_HPP
